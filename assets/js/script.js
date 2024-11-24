741e68dc009409929368d8dbc4015b65463f20fa
Tripta Grey <tripta.grey@gmail.com>
2024-11-24 14:25:24 +0000

    add function to add and remove to do with btn click event listener

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,48 @@
-console.log("connected");
+//create references to elements: txtBox, addBtn, clrBtn, delBtn, doneBtn
+const addToDoBtn = document.getElementById('addToDoBtn');
+const listTextInput = document.getElementById('listTextInput');
+const toDoList = document.getElementById('toDoList');
+
+//add click event listener to addToDoBtn
+addToDoBtn.addEventListener('click', addToDo);
+
+// create addToDo function to add to-do item to to-do list
+function addToDo(e) {
+    if (listTextInput.value) {
+        e.preventDefault(); // prevent default form submission
+        // get text from listTextInput
+        const toDoText = listTextInput.value;
+        // create new li element
+        const newToDoElement = document.createElement('li');
+        // add event listener to delete to-do item
+        newToDoElement.addEventListener('click', removeFromList);
+        
+        // add text to newToDoElement
+        newToDoElement.innerText = toDoText;
+        // add newToDoElement to toDoList
+        toDoList.appendChild(newToDoElement);
+        // clear text from listTextInput
+        listTextInput.value = '';
+        // focus on listTextInput
+        listTextInput.focus();
+    }
+    
+    
+}
+// chk:if there's txt in txtbox
+
+//create new li, newToDoElement
+
+//update list with newToDoElement
+
+// function to remove to-do item from list
+function removeFromList(e) {
+    //confirm if user wants to remove item
+    if(confirm('Are you sure you want to remove this item?')){
+    // remove to-do item from list
+    const listItem = e.target;
+    listItem.parentNode.removeChild(listItem);
+    //focus on listTextInput
+    listTextInput.focus();
+}
+}
