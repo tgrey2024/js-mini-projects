fdb2a4fe10d32ab192dadd72c9384b2c0393dfcc
Tripta Grey <tripta.grey@gmail.com>
2024-11-24 14:57:18 +0000

    add delete button per list item and clear list button

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,13 @@
 const addToDoBtn = document.getElementById('addToDoBtn');
 const listTextInput = document.getElementById('listTextInput');
 const toDoList = document.getElementById('toDoList');
+const clearBtn = document.getElementById('clearBtn');
 
 //add click event listener to addToDoBtn
 addToDoBtn.addEventListener('click', addToDo);
+
+//add click event listener to clearBtn
+clearBtn.addEventListener('click', clear);
 
 // create addToDo function to add to-do item to to-do list
 function addToDo(e) {
@@ -18,7 +22,7 @@ function addToDo(e) {
         newToDoElement.addEventListener('click', removeFromList);
         
         // add text to newToDoElement
-        newToDoElement.innerText = toDoText;
+        newToDoElement.innerHTML = toDoText + ' <button onClick="removeFromList">X</button>';
         // add newToDoElement to toDoList
         toDoList.appendChild(newToDoElement);
         // clear text from listTextInput
@@ -29,19 +33,25 @@ function addToDo(e) {
     
     
 }
-// chk:if there's txt in txtbox
-
-//create new li, newToDoElement
-
-//update list with newToDoElement
 
 // function to remove to-do item from list
 function removeFromList(e) {
     //confirm if user wants to remove item
     if(confirm('Are you sure you want to remove this item?')){
     // remove to-do item from list
-    const listItem = e.target;
+    const listItem = e.target.parentNode;
     listItem.parentNode.removeChild(listItem);
+    //focus on listTextInput
+    listTextInput.focus();
+}
+}
+
+// function to clear to-do list
+function clear() {
+    //confirm if user wants to clear list
+    if(confirm('Are you sure you want to clear the list?')){
+    // clear to-do list
+    toDoList.innerHTML = '';
     //focus on listTextInput
     listTextInput.focus();
 }
