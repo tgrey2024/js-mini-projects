9a2f5be7ce2d3dbd5d1edc09274c425d05e2bf8d
Tripta Grey <tripta.grey@gmail.com>
2024-12-11 20:09:44 +0000

    add navbar and notetaker placeholder code

---
+++ assets/js/noteTaker.js
@@ -0,0 +1,1 @@
+console.log('noteTaker.js loaded');
--- assets/js/toDo.js
+++ assets/js/toDo.js
@@ -19,7 +19,7 @@ function addToDo(e) {
         // create new li element
         const newToDoElement = document.createElement('li');
         // add text to newToDoElement and add delete button and done button
-        newToDoElement.innerHTML = `${toDoText} <button class="deleteBtn btn btn-bd-primary m-1" data-bs-toggle="popover">X</button><button class="doneBtn btn btn-bd-primary m-1" data-bs-toggle="popover">Done</button>`;
+        newToDoElement.innerHTML = `${toDoText} <button class="deleteBtn btn btn-bd-primary mx-1" data-bs-toggle="popover">X</button><button class="doneBtn btn btn-bd-primary mx-1" data-bs-toggle="popover">Done</button>`;
         // add newToDoElement to toDoList
         toDoList.appendChild(newToDoElement);
         // clear text from listTextInput
